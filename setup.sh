be96dc486b27b975c4cfc69356374976e79aaf67
Julian <julian@server.julianrunnels.com>
2020-04-13 16:44:25 -0700

    Updated values

--- setup.sh
+++ setup.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
-curl -sSL https://get.docker.com | sh
-sudo usermod -aG docker $USER
-sudo apt-get install -y libffi-dev libssl-dev
-sudo apt-get install -y python3 python3-pip
-sudo pip3 install docker-compose
+apt install docker
+usermod -aG docker $USER
+apt install -y libffi-dev libssl-dev
+apt install -y python3 python3-pip
+pip3 install docker-compose
+clear
+echo "Creating SSL CA and certificates"
 chmod +x create_ssl.sh
-sudo ./create_ssl.sh
+./create_ssl.sh
 echo "Docker and Docker-Compose have been installed, it is recommended to restart to make sure everything is installed correctly"
 echo "Would you like to restart?"
-select yn in "Y" "N"; do
+select yn in "Yes" "No"; do
     case $yn in
-        Y ) sudo reboot;;
-        N ) exit;;
+        Yes ) sudo reboot;;
+        No ) exit;;
     esac
 done
 
